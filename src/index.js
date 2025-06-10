4ec1bb0027ae4208cf05c16ccd4d84d748917c0f
AMOLIXS25 <wilmet.amaury@protonmail.com>
2025-06-10 11:49:34 +0200

    Projects Model Logic

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
+import { Project, ProjectList } from "./models/projectModel";
 import { Todo, TodoList } from "./models/todoModel";
 
 
 const index = () => {
-    const todoOne = new Todo('Clean room', '14-03-25', 'low', false);
-    const todoTwo = new Todo('Clean room 2', '14-03-25', 'low', true);
-    const todoThree = new Todo('Clean room 3', '14-03-25', 'low', true);
-    let todos = new TodoList();
-    todos.addTodo(todoOne);
-    todos.addTodo(todoTwo);
-    todos.addTodo(todoThree);
+    const todoOne = new Todo('Create youtube channel', '14-03-25', 'low', false);
+    const todoTwo = new Todo('Create formation', '14-03-25', 'low', true);
+    const todoThree = new Todo('And so on', '14-03-25', 'low', true);
+    let todosProjectOne = new TodoList();
+    todosProjectOne.addTodo(todoOne);
+    todosProjectOne.addTodo(todoTwo);
+    todosProjectOne.addTodo(todoThree);
+    let projectOne = new Project('Become rich');
+    projectOne.todoList = todosProjectOne;
+    let projectListOne = new ProjectList();
+    projectListOne.addProject(projectOne);
+    console.log(projectListOne.getProject(projectOne.id));
+    
+    console.log(projectListOne.getAllProjects());
+
 }
 
 index();
--- src/models/projectModel.js
+++ src/models/projectModel.js
@@ -1,9 +1,60 @@
 import { v4 as uuidv4 } from "uuid";
+import { TodoList } from "./todoModel";
 
 export class Project {
     #id;
+    #title;
+    #todoList;
+
+    constructor(title) {
+        this.#id = uuidv4();
+        this.#title = title;
+        this.#todoList = new TodoList();
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get todoList() {
+        return this.#todoList;
+    }
+
+    set todoList(newTodoList) {
+        this.#todoList = newTodoList;
+    }
 }
 
 export class ProjectList {
+    #projects;
 
+    constructor() {
+        this.#projects = [new Project('default')]
+    }
+
+    addProject(newProject) {
+        this.#projects.push(newProject);
+    }
+
+    getAllProjects() {
+        return this.#projects;
+    }
+
+    getProject(projectId) {
+        const result = this.#projects.filter((item) => {
+            return item.id == projectId;
+        });
+        return result[0];
+    }
+
+    updateProject(projectToUpdate, newProject) {
+        const indexOfPojectToUpdate = this.#projects.indexOf(projectToUpdate);
+        this.#projects[indexOfPojectToUpdate] = newProject;
+    }
+
+    deleteProject(projectId) {
+        this.#projects = this.#projects.filter((item) => {
+            return item.id !== projectId;
+        })
+    }
 }
